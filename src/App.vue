c6aa42356eac67ae696aafebac601b122f9c0737
Alexey Krasnoperov <20628763+alexey-k9v@users.noreply.github.com>
2022-02-01 11:42:13 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <PressReader />
+  </div>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld.vue";
+import PressReader from "./components/PressReader.vue";
+
+export default {
+  name: "App",
+  components: {
+    //HelloWorld,
+    PressReader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
